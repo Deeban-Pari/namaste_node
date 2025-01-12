8c4b7cb9eebd474974a9db2f1420c40e576eda93
Deeban-Pari <deebanpari2001@gmail.com>
2025-01-12 23:27:57 +0530

    devTinder | Added first endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const connectDB = require('./config/database');
+const user = require('./models/user');
 
 const app = express();
 
-app.use('/test',(req,res)=>{
-    res.send('Hello from the server');
+
+app.post('/signup',(req,res)=>{
+    try{
+        const saveUser = new user({
+            firstName : "Deeban",
+            lastName : "Pari",
+            emailId : "deeban@gmail.com",
+            password : "Testing@123"
+        });
+        saveUser.save();
+        res.send('User Added successfully');
+    }catch(err){
+        res.send("Error:",err);
+    }
 });
 
-app.listen(3000,()=>{
-    console.log('Server running and listening on port 3000');
+
+connectDB().then(()=>{
+    console.log("Database Connected");
+    app.listen(3000,()=>{
+        console.log('Server running and listening on port 3000');
+    });
+}).catch(err=>{
+    console.error("Error in connecting DB",err);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://deebanpari2001:zsZhPqE2LtGPH6Ku<@cluster0.em9ym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+const connectDB = async()=>{
+    await mongoose.connect('mongodb+srv://deebanpari2001:zsZhPqE2LtGPH6Ku@cluster0.em9ym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/devTinder');
+};
+
+module.exports = connectDB;
